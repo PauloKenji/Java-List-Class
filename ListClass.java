934eb5723a92e20b3014d23911667d64554b726f
Paulo Kenji <paulomuneischi@gmail.com>
2022-06-04 13:21:06 -0300

    refactor: refactor ListClass methods

--- ListClass.java
+++ ListClass.java
@@ -1,20 +1,40 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Vector;
 
 public class ListClass<T> {    
     
     private List<T> list = new ArrayList<>();
+    
 
     public void addElement(T element){
         list.add(element);
     }
 
     public void removeByIndex(int index){
-        list.remove(list.get(index));
+        if(index < list.size()){
+            list.remove(list.get(index));
+        }else System.out.println("Não há um elemento neste index");
     }
 
-    public void removeByElement(T element){
-        list.remove(element);
+    public void removeByElement(T element){        
+        if(hasThisElement(element)){
+            list.remove(element);
+        }else System.out.println("Não há este elemento na lista");        
+    }
+
+    public boolean hasThisElement(T element){
+        
+        Iterator<T> iteratorlist = list.iterator();
+
+        while(iteratorlist.hasNext()){
+            if(iteratorlist.next() == element){
+                return true;
+            }
+        }
+        return false;
     }
 
     public T getElementById(int index){
@@ -26,11 +46,16 @@ public class ListClass<T> {
         list.add(index, element);
     }
 
-    public List getAll(){
-        return list;
-    }
-
     public int getSize(){
         return list.size();
+    }    
+
+    public String getStringOfAllElements(){
+        Vector<T> vector = new Vector<>(list);
+        return vector.toString();
+    }
+
+    public void sort(){
+        Collections.sort(list, null);
     }
 }
--- MainList.java
+++ MainList.java
@@ -3,31 +3,30 @@ public class MainList {
     public static void main(String[] args) {
         ListClass myList = new ListClass<String>();
 
-        myList.addElement("uepa");
-        myList.addElement("sla");
-        myList.addElement("sasagueio");
+        myList.addElement("Bola");
+        myList.addElement("Armando");
+        myList.addElement("Castor");
 
-        myList.removeByIndex(1);
+        System.out.println(myList.getStringOfAllElements());
 
-        System.out.println(showAllElement(myList));
+        myList.sort();
 
-        myList.removeByElement("uepa");
-        
-        System.out.println(showAllElement(myList));
+        System.out.println(myList.getStringOfAllElements());
+
+        myList.removeByIndex(2);
+
+        System.out.println(myList.getStringOfAllElements());        
+
+        myList.removeByElement("Bola");
+
+        System.out.println(myList.getStringOfAllElements());  
 
         myList.update(0, "memers");
 
-        System.out.println(showAllElement(myList));
-    }
+        System.out.println(myList.getStringOfAllElements());  
 
-    public static String showAllElement(ListClass list){
-        String stringOfAllElements = "";
-
-        for(int i = 0; i < list.getSize(); i++){
-            stringOfAllElements += list.getElementById(i)+" ";
-        }
         
-        return stringOfAllElements; 
     }
+
     
 }
